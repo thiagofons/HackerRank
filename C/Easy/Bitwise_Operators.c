e630decedb791c3b7b136997d7c8cb65b4acde9b
Thiago Arruda <thiago_arruda@id.uff.br>
2021-12-17 15:39:44 -0300

    Some easy solving

---
+++ C/Easy/Bitwise_Operators.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+//Complete the following function.
+
+
+void calculate_the_maximum(int n, int k) {
+    int and, or, xor;
+    
+    for(int i = n; i <= k; i++){
+        for(int j = i + 1; j <= k; j++){
+            if(i != j){
+
+            }
+        }
+    }
+}
+
+int main() {
+    int n, k;
+  
+    scanf("%d %d", &n, &k);
+    calculate_the_maximum(n, k);
+ 
+    return 0;
+}
