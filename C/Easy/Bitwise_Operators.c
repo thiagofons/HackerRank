a315ab6563eb70e70a2d3089b6dabe5eddd58226
Thiago Arruda <thiago_arruda@id.uff.br>
2022-01-28 16:52:06 -0300

    New Exercises

--- C/Easy/Bitwise_Operators.c
+++ C/Easy/Bitwise_Operators.c
@@ -6,15 +6,24 @@
 
 
 void calculate_the_maximum(int n, int k) {
-    int and, or, xor;
-    
-    for(int i = n; i <= k; i++){
-        for(int j = i + 1; j <= k; j++){
-            if(i != j){
+    int initial = 1, final = n, and = 0, or = 0, xor = 0, test;
 
-            }
+    for(int i = 1; i < n; i++){
+        for(int j = i + 1; j <= n; j++){
+            test = i & j;
+            if(test > and && test < k)
+                and = test;
+            
+            test = i | j;
+            if(test > or && test < k)
+                or = test;
+
+            test = i ^ j;
+            if(test > xor && test < k)
+                xor = test;
         }
     }
+    printf("%d\n%d\n%d\n", and, or, xor);
 }
 
 int main() {
